009cc251d9bc78f7c260bb3f86eff7b93412a0cb
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2025-10-29 22:53:51 -0400

    fix test

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
   it "POST /session creates/returns a user" do
-    post "/session", params: { provider: 'guest', display_name: 'Matt' }
+    post "/session", params: { provider: 'guest', display_name: 'Matt' }, as: :json
     expect(response).to have_http_status(:ok)
     body = JSON.parse(response.body)
     expect(body['display_name']).to eq('Matt')
